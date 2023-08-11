27e58e2db86d32f46c299f5d0c7dafb7dcd1e5a3
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-08-10 17:19:56 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,11 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+int main(void)
+{
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, message, sizeof(message) - 1);
+return 1;
 }
 
