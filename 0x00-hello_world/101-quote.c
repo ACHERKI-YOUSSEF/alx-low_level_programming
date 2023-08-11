1100a26863ff3f3e53a9ee7e5f8061ae138e6093
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-08-10 17:25:02 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(2, message, sizeof(message) - 1);
-return 1;
+write(2, msg, sizeof(msg) - 1);
+return (1);
 }
 
