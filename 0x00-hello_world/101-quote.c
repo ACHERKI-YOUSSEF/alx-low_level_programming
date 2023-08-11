e53e6d2d1730ccf2e0854c792722f834b719817f
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-08-10 17:06:19 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
 * main - Entry point
 *
 * Return: Always 1 (Success)
 */
-#include <unistd.h>
 
 int main() {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    char message[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
     write(2, message, sizeof(message) - 1);
     return 1;
 }
