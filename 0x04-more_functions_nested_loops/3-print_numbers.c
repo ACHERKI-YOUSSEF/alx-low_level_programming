10303451538530ab532ac51516bf15085c712546
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-08-17 16:33:28 -0700

    c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = 48; ch <= '57; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
+
