5b4cafb1e589b5a287eb18f5f3ccaa844b19ee95
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-08-16 18:16:49 -0700

    c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,10 @@ int print_last_digit(int n)
 	int a;
 
 	if (n < 0)
-	{
-	n = -n;
-	a = n % 10;
-	_putchar(a + '0');
-	}
+	a = -1 * (n % 10);
 	else
-	{
 	a = n % 10;
 	_putchar(a + '0');
-	}
 	return (a);
 }
 
