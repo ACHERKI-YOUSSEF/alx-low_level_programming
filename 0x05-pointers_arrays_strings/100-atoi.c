20afc09b679bd1ca5ea8d7b7b0014d88d37056d7
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-08-21 17:52:02 -0700

    c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, a, n, len, f, dgt;
+
+	i = 0;
+	a = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	dgt = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++a;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (a % 2)
+				dgt = -dgt;
+			n = n * 10 + dgt;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
