ffe30cd1443c8cc1eacc6572c5fafe41478a15b6
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2023-08-24 18:47:02 -0700

    c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rotation by 13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char str2t[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == str1[j])
+			{
+				s[i] = str2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
